dc9a1dca99d44a1462f33fe1303e3e36d0cbf95a
Inniag <gianni.klesse@gmx.de>
2016-10-19 14:51:41 +0100

    Some code for testing Google Test.

---
+++ test/google-test-tutorial/whattotest.cpp
@@ -0,0 +1,11 @@
+// whattotest.cpp
+#include <cmath>
+ 
+double squareRoot(const double a) {
+    double b = sqrt(a);
+    if(b != b) { // nan check
+        return -1.0;
+    }else{
+        return sqrt(a);
+    }
+}
