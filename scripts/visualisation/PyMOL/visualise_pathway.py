7de9494bd3d83754205bd79b5df0fa0762ad09cf
Inniag <gianni.klesse@gmx.de>
2018-01-22 21:11:00 +0000

    Added convience functions for loading and displaying OBJ objects, refactord classes to have private attributes, added comments and removed superfluous functions.

--- scripts/visualisation/PyMOL/visualise_pathway.py
+++ scripts/visualisation/PyMOL/visualise_pathway.py
@@ -1,6 +1,7 @@
-
 from pymol import cmd, stored
-from pymol.cgo import *
+
+#from wobj import draw_wobj
+import wobj as wobj
 
 
 #cmd.bg_color(color = "white")
@@ -36,56 +37,15 @@ sel_pore_facing = "b > 0.5"
 cmd.show("sticks", sel_pore_facing)
 cmd.color("yellow", sel_pore_facing)
 
+
 ###############################################################################
 # ADD PORE SURFACE
 ###############################################################################
 
 
-cmd.hide("all")
+# read OBJ data from file:
+obj = wobj.import_wobj("output.obj")
 
+# draw all groups in the OBJ file:
+wobj.draw_wobj(obj)
 
-#obj = [ BEGIN, TRIANGLES ]
-#obj.extend([COLOR, 1, 0, 0, VERTEX, 0, 0, 0])
-#obj.extend([COLOR, 1, 1, 0, VERTEX, 1, 0, 0])
-#obj.extend([COLOR, 1, 0, 1, VERTEX, 0, 1, 0])
-#obj.extend([END])
-
-
-
-
-
-#obj = [
-#   BEGIN, LINES,
-#   COLOR, 1.0, 1.0, 1.0,
- 
-#   VERTEX, 0.0, 0.0, 0.0,
-#   VERTEX, 1.0, 0.0, 0.0,
-# 
-#   VERTEX, 0.0, 0.0, 0.0,
-#   VERTEX, 0.0, 2.0, 0.0,
-# 
-#   VERTEX, 0.0, 0.0, 0.0,
-#   VERTEX, 00, 0.0, 3.0,
-# 
-#   END
-#   ]                                                                                            
-
-
-
-def triangle ():
-    name="test";
-    obj=[ BEGIN, TRIANGLES ]
-    obj.extend([COLOR, 1.0, 0.0, 0.0, VERTEX, 0.0, 0.0, 0.0])
-    obj.extend([COLOR, 1.0, 0.0, 0.0, VERTEX, 2.0, 0.0, 0.0])
-    obj.extend([COLOR, 1.0, 0.0, 0.0, VERTEX, 0.0, 2.0, 0.0])
-    obj.extend([END])
-    cmd.delete("test")
-    cmd.load_cgo(obj,"test")
-
-    print obj
-    
-    return
-cmd.extend("triangle",triangle)
-
-
-triangle()
--- scripts/visualisation/PyMOL/wobj.py
+++ scripts/visualisation/PyMOL/wobj.py
@@ -2,14 +2,18 @@ from pymol import cmd
 from pymol.cgo import *
 
 
-
 class WavefrontMtlImporter:
+    """ Reads material specifications from Wavefront MTL files.
+    
+    Currently only supports reading ambient (Ka), diffuse (Kd), and specular
+    (Ks) colours from each material definition introduces with newmtl. 
+    """
 
     # dictionary of materials:
-    materials = dict()
+    __materials = dict()
 
     # most recently read material name:
-    crnt_material = ""
+    __crnt_material = ""
 
 
     def read(self, filename):
@@ -18,210 +22,316 @@ class WavefrontMtlImporter:
         with open(filename, "r") as f:
 
             # file is parsed line by line:
-            for line in f: self.parse_line(line)
+            for line in f: self.__parse_line(line)
         
         # return materials dictionary:
-        return self.materials
+        return self.__materials
 
 
-    def parse_line(self, line):
+    def __parse_line(self, line):
+        """ Parses one line from an MTL file.
+
+        Currently only supports newmtl, Ka, Kd, and Ks records. Depending on which
+        of these records in encountered, the corresponding parse function is 
+        called to further process the line. If an unknown record is encountered,
+        this method will throw an exception.
+
+        Args:
+            line: a string read from an MTL file
+        """
 
         # split line at space:
         split_line = line.split(" ")
     
         # what type of record does the lien contain?
-        if split_line[0] == "newmtl": self.parse_newmtl_line(split_line)
-        elif split_line[0] == "Ka": self.parse_ka_line(split_line)
-        elif split_line[0] == "Kd": self.parse_kd_line(split_line)
-        elif split_line[0] == "Ks": self.parse_ks_line(split_line)
+        if split_line[0] == "newmtl": self.__parse_newmtl_line(split_line)
+        elif split_line[0] == "Ka": self.__parse_ka_line(split_line)
+        elif split_line[0] == "Kd": self.__parse_kd_line(split_line)
+        elif split_line[0] == "Ks": self.__parse_ks_line(split_line)
         else: raise Exception("Encountered unknown record type: " + line)
 
 
-    def parse_newmtl_line(self, split_line):
+    def __parse_newmtl_line(self, split_line):
+        """ Parses a newmtl record read from a MTL file.
+
+        This will create a dictionary for the new material and also change the 
+        current matrial member variable accordingly.
+
+        Args:
+            split_line: list of strings created by splitting the MTL line at spaces
+        """
 
         # change current material:
-        self.crnt_material = split_line[1].rstrip()
+        self.__crnt_material = split_line[1].rstrip()
 
         # add current material to dictionary:
-        self.materials[self.crnt_material] = dict()
+        self.__materials[self.__crnt_material] = dict()
 
 
-    def parse_ka_line(self, split_line):
+    def __parse_ka_line(self, split_line):
+        """ Parses an ambient colour record read from a MTL file.
 
-        # add ambient colour value to materials dictionary:
-        self.materials[self.crnt_material]["Ka"] = map(float, split_line[1:4])
+        This will add an ambient colour definition to the current material.
 
-
-    def parse_kd_line(self, split_line):
-
-        # add ambient colour value to materials dictionary:
-        self.materials[self.crnt_material]["Kd"] = map(float, split_line[1:4])
-
-
-    def parse_ks_line(self, split_line):
+        Args:
+            split_line: list of strings created by splitting the MTL line at spaces
+        """
 
         # add ambient colour value to materials dictionary:
-        self.materials[self.crnt_material]["Ks"] = map(float, split_line[1:4])
+        self.__materials[self.__crnt_material]["Ka"] = map(float, split_line[1:4])
 
+
+    def __parse_kd_line(self, split_line):
+        """ Parses a diffuse colour record read from a MTL file.
+
+        This will add an diffuse colour definition to the current material.
+
+        Args:
+            split_line: list of strings created by splitting the MTL line at spaces
+        """
+
+        # add ambient colour value to materials dictionary:
+        self.__materials[self.__crnt_material]["Kd"] = map(float, split_line[1:4])
+
+
+    def __parse_ks_line(self, split_line):
+        """ Parses a specular colour record read from a MTL file.
+
+        This will add an specular colour definition to the current material.
+
+        Args:
+            split_line: list of strings created by splitting the MTL line at spaces
+        """
+
+        # add ambient colour value to materials dictionary:
+        self.__materials[self.__crnt_material]["Ks"] = map(float, split_line[1:4])
 
 
 
 class WavefrontObjImporter:
+    """ Reads scene data from Wavefront OBJ files.
+    
+    Currently supports reading of vertices and vertex normals, but not texture
+    vertices. Expects every face to have a vertex and vertex normal index 
+    specification.
+    """
    
     # dictionary of faces in groups:
-    groups = dict()
+    __groups = dict()
 
     # dictionary of materials read from MTL file:
-    materials = dict()
+    __materials = dict()
 
     # lists for vertices and vertex normals:
-    vertices = []
-    normals = []
-
-    # faces represented as lists of vertex, texture, and vertex normal indices: 
-    vertex_indices = []
-    vertex_normal_indices = []
-
+    __vertices = []
+    __normals = []
 
     # keep track of object, group, and material associated with a face:
-    crnt_object = ""
-    crnt_group = ""
-    crnt_material = ""
+    __crnt_object = ""
+    __crnt_group = ""
+    __crnt_material = ""
 
     
     def read(self, filename):
+        """ Reads data from a Wavefront OBJ file.
+
+        Args:
+            filename: name of the OBJ file to read
+
+        Returns:
+            dict: a dictionary of Compiled Graphics Objects
+        """
 
         # read data from OBJ file:
         with open(filename, "r") as f:
 
             # file is parsed line by line:
-            for line in f: self.parse_line(line)
+            for line in f: self.__parse_line(line)
 
         # create a dictionary for the CGO objects to be returned:
         cgo = dict()
 
         # loop over groups:
-        for key in self.groups:
+        for key in self.__groups:
        
             # add faces to CGO object:
-            cgo[key] = self.create_cgo_object(key)
-
+            cgo[key] = self.__create_cgo_object(key)
 
         # return dictionary of cgo objects:
         return cgo
-
-
-
-
-
+ 
     
-    
-    
-    def parse_line(self, line):
+    def __parse_line(self, line):
+        """ Parses a single line read from an OBJ file.
+
+        This will split the line string at spaces and check which type of 
+        record the line contains. It will then call the appropriate line 
+        parsing function.
+
+        Args:
+            line: string representing line from OBJ file
+        """
 
         # split string at space:
         split_line = line.split(" ")
 
         # determine type of record:
         if split_line[0] == "#": return           # ignore comments
-        elif split_line[0] == "mtllib": self.parse_mtllib_line(split_line) 
-        elif split_line[0] == "o": self.parse_o_line(split_line)
-        elif split_line[0] == "g": self.parse_g_line(split_line)
-        elif split_line[0] == "usemtl": self.parse_usemtl_line(split_line)
-        elif split_line[0] == "v": self.parse_v_line(split_line)
-        elif split_line[0] == "vn": self.parse_vn_line(split_line)
-        elif split_line[0] == "f": self.parse_f_line(split_line)
+        elif split_line[0] == "mtllib": self.__parse_mtllib_line(split_line) 
+        elif split_line[0] == "o": self.__parse_o_line(split_line)
+        elif split_line[0] == "g": self.__parse_g_line(split_line)
+        elif split_line[0] == "usemtl": self.__parse_usemtl_line(split_line)
+        elif split_line[0] == "v": self.__parse_v_line(split_line)
+        elif split_line[0] == "vn": self.__parse_vn_line(split_line)
+        elif split_line[0] == "f": self.__parse_f_line(split_line)
         elif split_line[0] == "\n": return        # ignore empty lines
         else: raise Exception("Encountered unknorn record type: " + line)
         
     
-    def parse_mtllib_line(self, split_line):
+    def __parse_mtllib_line(self, split_line):
+        """ Parses a mtllib record read from an OBJ file.
+
+        This will use the WavefrontMtlImporter class to parse the MTL file
+        specified in the record. The resulting materials are then added to an
+        internal dictionary.
+
+        Args:
+            split_line: list of strings created by splitting line at spaces
+        """
 
         mtl_importer = WavefrontMtlImporter()
-        self.materials = mtl_importer.read(split_line[1].rstrip())
+        self.__materials = mtl_importer.read(split_line[1].rstrip())
 
      
-    def parse_o_line(self, split_line):
+    def __parse_o_line(self, split_line):
+        """ Parses an o record read from an OBJ file.
+
+        This will simply change the current object variable.
+
+        Args:
+            split_line: list of strings created by splitting line at spaces
+        """
         
         # new current object:
-        crnt_object = split_line[1]
+        __crnt_object = split_line[1]
    
 
-    def parse_usemtl_line(self, split_line):
+    def __parse_usemtl_line(self, split_line):
+        """ Parses an usemtl record read from an OBJ file.
+
+        This will change the current material to the value contained in the
+        given record.
+
+        Args:
+            split_line: list of strings created by splitting line at spaces
+        """
         
         # new current material:
-        self.crnt_material = split_line[1].rstrip()
+        self.__crnt_material = split_line[1].rstrip()
     
 
-    def parse_g_line(self, split_line):
+    def __parse_g_line(self, split_line):
+        """ Parses a g record read from an OBJ file.
+
+        This changes the current group name and also creates a dictionary 
+        entry for this group name. Inside this dictionary, an empty list of
+        faces is created.
+
+        Args:
+            split_line: list of strings created by splitting line at spaces
+        """
     
         # new current group:
-        self.crnt_group = split_line[1].rstrip()
+        self.__crnt_group = split_line[1].rstrip()
         
         # add group to group dictionary:
-        self.groups[self.crnt_group] = dict()
-        self.groups[self.crnt_group]["faces"] = []
+        self.__groups[self.__crnt_group] = dict()
+        self.__groups[self.__crnt_group]["faces"] = []
 
 
+    def __parse_v_line(self, split_line):
+        """ Parses a v record read from an OBJ file.
 
-    def parse_v_line(self, split_line):
-    
+        This adds a new 3-vector of floats to the internal list of vertices.        
+
+        Args:
+            split_line: list of strings created by splitting line at spaces
+        """
+         
         # append vertex 3-vector to list of vertices:
-        self.vertices.append(map(float, split_line[1:4]))
+        self.__vertices.append(map(float, split_line[1:4]))
 
     
-    def parse_vn_line(self, split_line):
+    def __parse_vn_line(self, split_line):
+        """ Parses a vn record read from an OBJ file.
+
+        This adds a new 3-vector of floats to the internal list of vertex
+        normals.
+
+        Args:
+            split_line: list of strings created by splitting line at spaces
+        """
     
         # append vertex normal 3-vector to list of vertices:
-        self.normals.append(map(float, split_line[1:4]))
+        self.__normals.append(map(float, split_line[1:4]))
     
 
-    def parse_f_line(self, split_line):
+    def __parse_f_line(self, split_line):
+        """ Parses a f record read from an OBJ file.
 
+        This adds a new entry to the list of faces for the current group. A
+        face entry consists of two 3-vectors of vertex indices and vertex 
+        normal indices as well as the name of the current material.        
+
+        Args:
+            split_line: list of strings created by splitting line at spaces
+        """
 
         # split at slash to separate out vertex, texture and normal index: 
         all_indices = map(lambda x: x.split("/"), split_line[1:4])
 
         # create a face with material and index into vertex and normal lists:
         face = dict()
-        face["material"] = self.crnt_material
+        face["material"] = self.__crnt_material
         face["vert_idx"] = map(int, map(lambda x: x[0], all_indices)) 
         face["norm_idx"] = map(int, map(lambda x: x[2], all_indices)) 
  
         # append face to list of faces for the current group:
-        self.groups[self.crnt_group]["faces"].append(face)
+        self.__groups[self.__crnt_group]["faces"].append(face)
+    
 
+    def __create_cgo_object(self, groupname):
+        """ Creates a Compiled Graphics Object from the fully parsed OBJ file.
 
-        # append each index type to individual list:
-        # TODO remove
-        self.vertex_indices.append(
-            map(int, map(lambda x: x[0],
-            all_indices)) )
-        self.vertex_normal_indices.append(
-            map(int, map(lambda x: x[2],
-            all_indices)) )
+        This function is called after all records have been read from an OBJ 
+        file and associated MTL file. It goes through the list of all faces
+        for the given group and creates a CGO TRIANGLE entry for each. 
 
+        Args:
+            groupname: string, must be a key in the groups dictionary
 
-    def create_cgo_object(self, groupname):
+        Returns:
+            CGO: a Compiled Graphics Object compatible with PyMOL
+        """
 
         # create a CGO object:
         cgo_object = [ BEGIN, TRIANGLES ]
 
         # loop over faces in group:
-        for face in self.groups[groupname]["faces"]:
+        for face in self.__groups[groupname]["faces"]:
 
             # obtain vertices (note index shift wrt OBJ file):
-            vertex_a = self.vertices[face["vert_idx"][0] - 1]
-            vertex_b = self.vertices[face["vert_idx"][1] - 1]
-            vertex_c = self.vertices[face["vert_idx"][2] - 1]
+            vertex_a = self.__vertices[face["vert_idx"][0] - 1]
+            vertex_b = self.__vertices[face["vert_idx"][1] - 1]
+            vertex_c = self.__vertices[face["vert_idx"][2] - 1]
             
             # obtain normals (note index shift wrt OBJ file):
-            normal_a = self.normals[face["norm_idx"][0] - 1]
-            normal_b = self.normals[face["norm_idx"][1] - 1]
-            normal_c = self.normals[face["norm_idx"][2] - 1]
+            normal_a = self.__normals[face["norm_idx"][0] - 1]
+            normal_b = self.__normals[face["norm_idx"][1] - 1]
+            normal_c = self.__normals[face["norm_idx"][2] - 1]
 
             # obtain ambient colour from material:
-            ka = self.materials[face["material"]]["Ka"]
+            ka = self.__materials[face["material"]]["Ka"]
 
             # add triangular vertices to CGO object:
             cgo_object.extend([
@@ -243,21 +353,49 @@ class WavefrontObjImporter:
 
         # return CGO obtject:
         return cgo_object
+
+
+def import_wobj(filename):
+    """ Reads data from OBJ file and returns a dictionary of CGO objects.
+
+    Args:
+        filename: Name of the OBJ file to load.
+
+    Returns:
+        dict: Dictionary of CGO objects where the keys are OBJ group names.
+    """   
  
+    # create OBJ importer:
+    obj_importer = WavefrontObjImporter()
+    
+    # import data from OBJ file:
+    return obj_importer.read(filename)
+
+
+def draw_wobj(obj, groupname = None):
+    """ Draws a named Compiled Graphics Object (CGO) from a given dictionary.
+   
+    Takes a dictionary of Compiled Graphics Objects and a string as input and
+    uses PyMOL functions to draw the CGO associated with a given string. If
+    the name is none (the default) all CGOs in the dictionary will be drawn.
+    This can then be manually hidden or shown using the PyMOL GUI.
+ 
+    Args:
+        obj: Dictionary of names and Compiled Graphics Objects
+        groupname: Name of the Compiled Graphics Object to draw
+    """   
+ 
+    # delete existing objects under this name:
+    cmd.delete(groupname)
+
+    # has a group name been specified?
+    if groupname != None:
         
+        # load the requested CGO object:
+        cmd.load_cgo(obj[groupname], groupname)
 
+    else:
 
-importer = WavefrontObjImporter()
-
-cmd.hide("all")
-
-
-cgo = importer.read("output.obj")
-
-print cgo.keys()
-
-cmd.delete("test")
-cmd.load_cgo(cgo["avg_pl_hydrophobicity"], "test")
-
-
-
+        # load all CGO objects in dictionary:
+        for key in obj.keys(): cmd.load_cgo(obj[key], key)
+    
