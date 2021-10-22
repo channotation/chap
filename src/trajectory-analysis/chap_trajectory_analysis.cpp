7a87e9e78d8f22f1e8a5c0a888c92f1b72b4e29d
DSeiferth <david.seiferth@dtc.ox.ac.uk>
2021-10-22 22:05:12 +0100

    debug comments

--- src/trajectory-analysis/chap_trajectory_analysis.cpp
+++ src/trajectory-analysis/chap_trajectory_analysis.cpp
@@ -1572,8 +1572,8 @@ ChapTrajectoryAnalysis::finishAnalysis(int numFrames)
     {
         //throw std::runtime_error("Number of frames read does not equal number"
         //"of frames analyised.");
-        std::cout << "linesRead" << linesRead <<std::endl;
-        std::cout << "numFrames" << numFrames <<std::endl;
+        std::cout << "linesRead " << linesRead <<std::endl;
+        std::cout << "numFrames " << numFrames <<std::endl;
     }
 
 
